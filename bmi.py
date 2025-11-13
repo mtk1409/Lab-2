284076f2b1e0ec726fc24f252a4f0120b6c1652e
MayThinKhant <MAYTHIN.24@ichat.sp.edu.sg>
2025-11-13 10:21:09 +0800

    Modified calculate_bmi() to return integer

--- bmi.py
+++ bmi.py
@@ -5,12 +5,15 @@ def calculate_bmi(height, weight):
     print("BMI = ", f"{BMI: .2f}")
     if BMI < 18.5:
         print("Under Weight")
+        return -1
     elif BMI > 25.0:
         print("Over Weight")
+        return 1
     else: 
         print("Normal Weight")
+        return 0
 
 
-calculate_bmi(weight=37, height=1.73)
-calculate_bmi(weight=97, height=1.73)
-calculate_bmi(weight=57, height=1.73)
+print(calculate_bmi(weight=37, height=1.73))
+print(calculate_bmi(weight=97, height=1.73))
+print(calculate_bmi(weight=57, height=1.73))
